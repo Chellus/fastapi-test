a50d84f0e5cacf4a081b1496af9d7eef8d675f0b
Chellus <elias4845@gmail.com>
2025-04-20 19:54:56 -0400

    removed age field in users table

--- app/models_db/user.py
+++ app/models_db/user.py
@@ -1,10 +1,8 @@
-from app.db.database import create_db_and_tables
 from sqlmodel import Field, SQLModel
 
 class Users(SQLModel, table=True):
     user_id: int | None = Field(default=None, primary_key=True, index=True)
     name: str
-    username: str
-    email: str
-    age: int
+    username: str = Field(default=None, unique=True, index=True)
+    email: str = Field(default=None, unique=True)
     password: str
