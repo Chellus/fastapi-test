aeb47cabf1c2d00a70e9a166982e5a4129e2bc1c
Chellus <elias4845@gmail.com>
2025-04-20 19:53:34 -0400

    get_session function added

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
 from app.utils.config import settings
 
 db_url = settings.DATABASE_URL
@@ -7,3 +7,7 @@ engine = create_engine(db_url, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
